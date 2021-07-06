25016fdb907f4675795a1fdc1f2539341d4ddb85
Fernando Aires <github@friarhob.33mail.com>
2021-07-06 20:59:34 -0300

    Implementing reset() function

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let startTime = 0;
+
+function reset()
+{
+    document.getElementById("hour").innerHTML = "00";
+    document.getElementById("minute").innerHTML = "00";
+    document.getElementById("second").innerHTML = "00";
+}
