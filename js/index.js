e8aa92ba58be024650fde2f43636977db54aa6da
Fernando Aires <github@friarhob.33mail.com>
2021-07-07 13:07:55 -0300

    Fixing reset

--- js/index.js
+++ js/index.js
@@ -5,40 +5,38 @@ function reset() {
     document.getElementById("hour").innerHTML = "00";
     document.getElementById("minute").innerHTML = "00";
     document.getElementById("second").innerHTML = "00";
+    clearInterval(cron);
 }
 
 function start() {
     endTime = Date.now();
-    endTime += parseInt(document.getElementById("pomodoro-minutes").value)*60*1000;
+    endTime += parseInt(document.getElementById("pomodoro-minutes").value) * 60 * 1000;
     cron = setInterval(() => {
         update();
     }, 100);
 }
 
-function update()
-{
+function update() {
     let milisseconds = endTime - Date.now();
 
-    if(milisseconds <= 0)
-    {
+    if (milisseconds <= 0) {
         clearInterval(cron); //to be updated - now it works just as a simple timer
         reset();
     } else {
-        let seconds = Math.floor(milisseconds/1000);
-        document.getElementById("second").innerHTML = format(seconds%60);
-    
-        let minutes = Math.floor(seconds/60);
-        document.getElementById("minute").innerHTML = format(minutes%60);
-    
-        let hours = Math.floor(minutes/60);
+        let seconds = Math.floor(milisseconds / 1000);
+        document.getElementById("second").innerHTML = format(seconds % 60);
+
+        let minutes = Math.floor(seconds / 60);
+        document.getElementById("minute").innerHTML = format(minutes % 60);
+
+        let hours = Math.floor(minutes / 60);
         document.getElementById("hour").innerHTML = format(hours);
-    
+
     }
 
 }
 
-function format(number)
-{
-    if(number >= 10) return ""+number;
-    return "0"+number;
+function format(number) {
+    if (number >= 10) return "" + number;
+    return "0" + number;
 }
