de5b3c46b670cf121c2cbc07eca3a0c8186dbb5b
Fernando Aires <github@friarhob.33mail.com>
2021-07-07 09:58:11 -0300

    prettier adjustments

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 let startTime = 0;
 
-function reset()
-{
+function reset() {
     document.getElementById("hour").innerHTML = "00";
     document.getElementById("minute").innerHTML = "00";
     document.getElementById("second").innerHTML = "00";
